98e5c5060813195f91e15d711f054de6dd1a2c58
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2022-02-02 11:54:25 +0100

    Update 1 - esercizio 1 pagina 48.asm

--- 1 - esercizio 1 pagina 48.asm
+++ 1 - esercizio 1 pagina 48.asm
@@ -1,9 +1,9 @@
-Assumendo che l’indirizzo base di A (B) sia in $s3 ($s4), e che la 
-dimensione dell’array A sia memorizzata in $s5, scrivere in 
-assembler le due seguenti porzioni di codice C:
+# Assumendo che l’indirizzo base di A (B) sia in $s3 ($s4), e che la 
+# dimensione dell’array A sia memorizzata in $s5, scrivere in 
+# assembler le due seguenti porzioni di codice C:
 
-Sommare tutti gli elementi di un array A di 100 interi che siano in 
-posizione pari
+# Sommare tutti gli elementi di un array A di 100 interi che siano in 
+# posizione pari
 
 # $s3 = &A[0]
 # $s5 = dim di A (100)
